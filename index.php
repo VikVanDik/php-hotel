0f0991c60035a87f9141fc98555fa57de6060d06
VikVanDik <95vlc17@gmail.com>
2023-11-01 17:55:35 +0100

    [add] : table on foreach loop

--- index.php
+++ index.php
@@ -49,14 +49,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Hotel</title>
 </head>
 <body>
-    <?php foreach ($hotels as $hotel): ?>
-    <div><?php echo $hotel['name'] ?></div>
-    <div><?php echo $hotel['description'] ?></div>
-    <div><?php echo $hotel['vote'] ?></div>
-    <div><?php echo $hotel['distance_to_center'] ?></div>
-    <?php endforeach; ?>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Voto</th>
+            <th scope="col">Parcheggio</th>
+            <th scope="col">Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel): ?>
+            <tr>
+            <th scope="row"><?php echo $hotel['name'] ?></th>
+            <td><?php echo $hotel['description'] ?></td>
+            <td><?php echo $hotel['vote'] ?></td>
+            <td><?php echo $hotel['parking'] ? 'Yes' : 'No' ?></td>
+            <td><?php echo $hotel['distance_to_center'] ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
